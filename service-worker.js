0396e5c5512b7efb70b538b6328acddf78f6ee7f
extonben <ben.exton@hotmail.co.uk>
2025-08-22 14:44:45 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+// service-worker.js
+
+const CACHE_NAME = 'expense-tracker-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+  'manifest.json'
+];
+
+// Install event: caches all the necessary assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache and caching assets.');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: serves cached assets when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached response if found
+        if (response) {
+          return response;
+        }
+        // If not found, fetch from network
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event: deletes old caches to free up space
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
